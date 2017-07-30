aff9d0121173a57cbb37751020027a058aced044
hypervisor <hypervisor@DESKTOP-8RMNN45>
2017-07-30 18:10:50 +0800

    --initial commit

---
+++ src/main/java/chapter2/bootmessaging/Sender.java
@@ -0,0 +1,23 @@
+package chapter2.bootmessaging;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component 
+class Sender {
+  @Autowired
+  RabbitMessagingTemplate template;
+  @Bean
+  Queue queue() {
+    return new Queue("TestQ", false);
+  }
+  public void send(String message) {
+	
+		    template.convertAndSend("TestQ", message);
+	  
+
+  }
+}
